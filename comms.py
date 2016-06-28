7a4fbad629d2ca4854ef741c709e0868574d4b34
Marten Soderberg <marten@martensoderberg.se>
2016-06-28 21:19:32 +0000

    much improved serial communication

--- comms.py
+++ comms.py
@@ -1,16 +1,30 @@
 import serial
+import time
 
 def loop():
-  ser = serial.Serial('/dev/ttyACM0', 9600)
+  ser = serial.Serial(
+    port = '/dev/ttyACM0',
+    baudrate = 9600,
+    parity = serial.PARITY_NONE,
+    stopbits = serial.STOPBITS_ONE,
+    bytesize = serial.EIGHTBITS,
+    timeout = 2,
+    xonxoff = False,
+    rtscts = False,
+    writeTimeout = 2
+  )
   i = 0
 
   while i < 100:
-    outString = b"Hello"
-    print("Sent: " + outString)
-    ser.write(outString)
-    result = ser.read()
-    print("Rcvd: " + result)
+    command = bytes([1])
+    print("Sent: 1")
+    ser.write(command)
+    time.sleep(0.05)
+    returnedBytes = ser.read(ser.inWaiting())
+    resultString = returnedBytes.decode("utf-8")
+    #result = int.from_bytes(returnedBytes, byteorder='big')
+    print("Rcvd: " + str(resultString))
     i = i + 1
-    ser.close()
+  ser.close()
 
 loop()
