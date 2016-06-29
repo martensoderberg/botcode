558ef5257d09f7a55bd40621b9b92e0f82446b51
Mårten Söderberg <marten@martensoderberg.se>
2016-06-29 21:37:37 +0200

    Updated comms.py

--- comms.py
+++ comms.py
@@ -1,9 +1,10 @@
 import serial
 import time
 
-def loop():
+def defineArduinoConnection(portNumber):
+  portName = "/dev/ttyACM" + str(portNumber)
   ser = serial.Serial(
-    port = '/dev/ttyACM0',
+    port = portName
     baudrate = 9600,
     parity = serial.PARITY_NONE,
     stopbits = serial.STOPBITS_ONE,
@@ -13,8 +14,22 @@ def loop():
     rtscts = False,
     writeTimeout = 2
   )
-  i = 0
+  return ser
 
+def tryArduinoConnection():
+  for portNumber in [1, 2, 3, 4]:
+    try:
+      ser = defineArduinoConnection(portNumber)
+      return (True, ser) # success
+    except:
+      continue
+  return (False, ser) # nothing found
+
+def main():
+  (success, ser) = tryArduinoConnection()
+  if not success:
+    print("Could not connect to Arduino, exiting")
+    exit(1)
   while i < 100:
     command = bytes([1])
     print("Sent: 1")
@@ -22,9 +37,9 @@ def loop():
     time.sleep(0.05)
     returnedBytes = ser.read(ser.inWaiting())
     resultString = returnedBytes.decode("utf-8")
-    #result = int.from_bytes(returnedBytes, byteorder='big')
     print("Rcvd: " + str(resultString))
     i = i + 1
   ser.close()
 
-loop()
+main()
+
