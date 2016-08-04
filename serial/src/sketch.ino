2ed8a7b989f5cdbbd84367a81ef3df8c84ac5c32
Marten Soderberg <marten@martensoderberg.se>
2016-08-04 22:00:26 +0000

    Started implementing LED support

--- serial/src/sketch.ino
+++ serial/src/sketch.ino
@@ -1,9 +1,18 @@
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN_NO 10
+
+// This initializes a "strip" of exactly 1 LED (so not a strip...)
+Adafruit_NeoPixel led = Adafruit_NeoPixel(1, LED_PIN_NO, NEO_GRB + NEO_KHZ800);
+
 String inputString = "";
 boolean stringComplete = false;
 
 void setup() {
   Serial.begin(9600);
   inputString.reserve(200); // reserves space
+  led.begin();
+  led.show();
 }
 
 void loop() {
@@ -14,6 +23,9 @@ void loop() {
     stringComplete = false;
     if (inputString.equals("Hello")) {
       Serial.print("Hey there handsome!");
+    } else if (inputString.startsWith("LED:")) {
+      // This message should be on the form "LED:r:g:b"
+
     } else {
       Serial.print("Ohai");
     }
