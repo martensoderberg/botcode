23c8dfcf85b5c998e5d146642e46e6c52309cce6
Marten Soderberg <marten@martensoderberg.se>
2016-08-04 22:30:22 +0000

    Stopped using String in the arduino code

--- serial/src/sketch.ino
+++ serial/src/sketch.ino
@@ -1,16 +1,18 @@
 #include <Adafruit_NeoPixel.h>
 
+#define MESSAGE_BUFFER_SIZE 100
+
 #define LED_PIN_NO 10
 
 // This initializes a "strip" of exactly 1 LED (so not a strip...)
 Adafruit_NeoPixel led = Adafruit_NeoPixel(1, LED_PIN_NO, NEO_GRB + NEO_KHZ800);
 
-String inputString = "";
+char msgBuf[MESSAGE_BUFFER_SIZE];
+int msgLen;
 boolean stringComplete = false;
 
 void setup() {
   Serial.begin(9600);
-  inputString.reserve(200); // reserves space
   led.begin();
   led.show();
 }
@@ -21,9 +23,11 @@ void loop() {
   // check if data has been sent from the computer
   if (stringComplete) {
     stringComplete = false;
-    if (inputString.equals("Hello")) {
+    if (strcmp(msgBuf,"Hello") == 0) {
+      // The message was "Hello"
       Serial.print("Hey there handsome!");
-    } else if (inputString.startsWith("LED:")) {
+    } else if (msgLen >= 4 && strncmp(msgBuf, "LED:", 4) == 0) {
+      // The message beegins with "LED:"
       // This message should be on the form "LED:r:g:b"
 
     } else {
@@ -31,7 +35,7 @@ void loop() {
     }
     byte stopByte = 0;
     Serial.write(stopByte);
-    inputString = "";
+    msgLen = 0; // reset the message length counter
   }
 }
 
@@ -39,9 +43,11 @@ void checkSerialPort() {
   while (Serial.available()) {
     char inChar = (char) Serial.read();
     if (inChar == 0) {
+      msgBuf[msgLen] = 0; // terminating character
       stringComplete = true;
     } else {
-      inputString += inChar;
+      msgBuf[msgLen] = inChar;
+      msgLen++;
     }
   }
 }
