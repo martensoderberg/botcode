dd994ef1a792cb82f10a5dd5138f7b2c88fb5595
Marten Soderberg <marten@martensoderberg.se>
2016-08-05 19:55:13 +0000

    Redid logging

--- comms.py
+++ comms.py
@@ -1,40 +1,46 @@
 import serial
 import time
+import datetime
 import threading
 
-def defineArduinoConnection(portName):
-  ser = serial.Serial(
-    port = portName,
-    baudrate = 9600,
-    parity = serial.PARITY_NONE,
-    stopbits = serial.STOPBITS_ONE,
-    bytesize = serial.EIGHTBITS,
-    timeout = 2,
-    xonxoff = False,
-    rtscts = False,
-    writeTimeout = 2
-  )
-  return ser
+startTime = datetime.datetime.now()
 
-def tryArduinoConnection():
-  for portNumber in [0, 1, 2, 3]:
-    try:
-      portName = "/dev/ttyACM" + str(portNumber)
-      print("Attempting to connect to arduino on " + portName + "... ",end="")
-      ser = defineArduinoConnection(portName)
-      print("succeded")
-      return (True, ser) # success
-    except:
-      print("failed")
-      continue
-  return (False, None) # nothing found
+def getTimestamp():
+  timeNow = datetime.datetime.now()
+  return str(timeNow - startTime)
 
 # This class handles communications with the arduino controller
 # over an already-established serial port (ser)
 class ArduinoCommsThread(threading.Thread):
-  def __init__(self, ser):
+  def __init__(self):
     threading.Thread.__init__(self)
-    self.ser = ser
+
+  def defineArduinoConnection(self, portName):
+    ser = serial.Serial(
+      port = portName,
+      baudrate = 9600,
+      parity = serial.PARITY_NONE,
+      stopbits = serial.STOPBITS_ONE,
+      bytesize = serial.EIGHTBITS,
+      timeout = 2,
+      xonxoff = False,
+      rtscts = False,
+      writeTimeout = 2
+    )
+    return ser
+
+  def tryArduinoConnection(self):
+    for portNumber in [0, 1, 2, 3]:
+      try:
+        portName = "/dev/ttyACM" + str(portNumber)
+        print(getTimestamp() + ": Attempting to connect to arduino on " + portName + "... ",end="")
+        ser = self.defineArduinoConnection(portName)
+        print("succeded")
+        return (True, ser) # success
+      except:
+        print("failed")
+        continue
+    return (False, None) # nothing found
 
   def sendMessage(self, message):
     ser = self.ser
@@ -54,26 +60,28 @@ class ArduinoCommsThread(threading.Thread):
     return frame.decode("utf-8")
 
   def run(self):
-    ser = self.ser
+    # Establish connection
+    (success, ser) = self.tryArduinoConnection()
+    while not success:
+      time.sleep(1)
+      (success, ser) = self.tryArduinoConnection()
+    self.ser = ser
+
     i = 0
-    while i < 100:
+    while i < 10000:
       if (i % 2) == 1:
         command = "LED:0:0:0"
       else:
         command = "LED:15:105:255"
-      print("Sent: " + command)
+      print(getTimestamp() + ": Sent: " + command)
       self.sendMessage(command)
       reply = self.receiveMessage()
-      print("Rcvd: " + reply)
+      print(getTimestamp() + ": Rcvd: " + reply)
       i = i + 1
     ser.close()
 
 def main():
-  (success, ser) = tryArduinoConnection()
-  if success:
-    commsThread = ArduinoCommsThread(ser)
-    commsThread.start()
-  else:
-    print("Could not connect to Arduino, exiting")
-    exit(1)
+  commsThread = ArduinoCommsThread()
+  commsThread.start()
+
 main()
